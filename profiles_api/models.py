0532854d2a2e98db1508f793afabbf2e02f9898a
Papanics <onicsbantu@gmail.com>
2022-05-23 22:10:10 +0800

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
@@ -40,7 +40,7 @@ class UserProfile(AbstractUser):
 
     objects = UserProfileManager()
 
-    USERNAME_FIELD: 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
 
